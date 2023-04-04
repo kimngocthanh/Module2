8b28edb765a8281ffe25d7e0815ed3ebcd496631
kimngocthanh <63952549+kimngocthanh@users.noreply.github.com>
2023-04-04 20:04:32 +0700

    main

--- src/ss3/bai_tap/ThemPhanTuVaoMang.java
+++ src/ss3/bai_tap/ThemPhanTuVaoMang.java
@@ -1,5 +1,6 @@
 package ss3.bai_tap;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ThemPhanTuVaoMang {
@@ -10,19 +11,24 @@ public class ThemPhanTuVaoMang {
         int[] mang = new int[doDai];
         int[] mangChen = new int[doDai + 1];
         for (int i = 0; i < doDai; i++) {
-            System.out.print("nhập phần tử thứ :" + (i + 1));
+            System.out.print("nhập phần tử thứ " + (i + 1)+":");
             mang[i] = scanner.nextInt();
         }
         System.out.println("nhập số muốn chèn :");
         int x = scanner.nextInt();
         System.out.println("nhập vị trí muốn chèn");
         int viTri = scanner.nextInt();
-        for (int i = doDai; i >viTri ; i++) {
-            mangChen[i]=mang[i-1];
+        for (int i = 0; i <doDai ; i++) {
+            if(i==viTri){
+                for (int j = viTri; j < mangChen.length-1; j++) {
+                    mangChen[i]=x;
+                    mangChen[j]=mang[j];
+                }
+            }else {
+                mangChen[i]=mang[i];
+            }
         }
-        mangChen[viTri]=x;
-        for (int i = 0; i < mangChen.length; i++) {
-            System.out.print(mangChen[i]);
-        }
+        System.out.println(Arrays.toString(mangChen));
+
     }
 }
